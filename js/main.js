4ed505819800b94f93d7b77539e8afe85e532af2
Amidou Kante <kamdiou@gmail.com>
2017-07-12 08:49:28 -0400

    started option to choose game mode

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ window.onload = function(){
     console.log("Game Ready"); 
     startGame() 
 }
-
+const gameMode = [
+    easy = {
+        speed: 3, 
+        birds: 1,
+        size: '300px',
+        bullets: 5
+    },
+]
 const birdOne = $('.bird1');
 
 function startGame(){
     setTimeout(function(){
-        $(birdOne).animate({left: `${Math.random()*1000}px`})
+        $(birdOne).animate({left: `${Math.random()*15000}px`})
         $(birdOne).animate({top: `${Math.random()*500}px`})
     },100)
 }
