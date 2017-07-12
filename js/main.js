00a2176c3ef8d3602531368f9f58825409b48a96
Amidou Kante <kamdiou@gmail.com>
2017-07-12 10:46:51 -0400

    able to make birds disappear

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const birdOne = $('.duck1');
 const birdTwo = $('.duck2');
 const gameMode = [
     easy = {
-        speed: 3, 
+        speed: 10, 
         birds: 1,
         size: '300px',
         bullets: 5,
@@ -32,15 +32,16 @@ function moveBird(difficulty){
     if(difficulty.birds >= 2){
         let birdsGroup = $('.gameBoard div')
         for(let i = 0; i <difficulty.birds; i++){
+            let currBird = birdsGroup[i];
             setTimeout(function(){
             randomDist = (Math.random() * (2100 - 1800) + 1800); //specify random range for the distance
             randomTop = (Math.random() * (500 - (-300)) + (-300)); //specify random range for the height
-                $(birdsGroup[i]).css({display: 'block'})
-                $(birdsGroup[i]).css({width: `${difficulty.size}`})
-                $(birdsGroup[i]).css({height: `${difficulty.size}`})
-                $(birdsGroup[i]).animate({left: `${randomDist}px`})
-                $(birdsGroup[i]).animate({top: `${randomTop}px`})
-                $(birdsGroup[i]).css({transition: `${difficulty.speed}s linear`})
+                $(currBird).css({display: 'block'})
+                $(currBird).css({width: `${difficulty.size}`})
+                $(currBird).css({height: `${difficulty.size}`})
+                $(currBird).animate({left: `${randomDist}px`})
+                $(currBird).animate({top: `${randomTop}px`})
+                $(currBird).css({transition: `${difficulty.speed}s linear`})
             },100)
         }
     }else {
@@ -56,5 +57,10 @@ function moveBird(difficulty){
 }
 $(birdOne).click(function(){
     console.log("Hit")
-    $(this).css({display: "none"})
+    // localStorage.setItem(score, "500")
+    $(this).animate({transition: '0s'})
+    $(this).css({background: "url('./images/shotDuck.png') center no-repeat"})
+    setTimeout(()=>{
+        $(this).css({opacity: '0'});
+    },700)
 })
