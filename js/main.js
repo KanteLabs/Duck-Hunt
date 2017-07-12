de609d2aa2247b1382a3ba00e9cc0fb841d186c1
Amidou Kante <kamdiou@gmail.com>
2017-07-12 09:36:52 -0400

    fixed random height and top

--- js/main.js
+++ js/main.js
@@ -2,38 +2,40 @@ window.onload = function(){
     console.log("Game Ready"); 
     startGame() 
 }
+
+const birdOne = $('.duck1');
+const birdTwo = $('.duck2');
 const gameMode = [
     easy = {
         speed: 3, 
         birds: 1,
         size: '300px',
         bullets: 5,
-    },
-    medium = {
+    },medium = {
         speed: 2, 
         birds: 2,
         size: '200px',
         bullets: 3
-    },
-    hard = {
+    },hard = {
         speed: 1, 
         birds: 3,
         size: '150px',
         bullets: 1
     }
 ]
-const birdOne = $('.bird1');
 
 function startGame(){
-    let difficulty = prompt("Easy, Medium or Hard").toLowerCase();
+    let difficulty = 'easy'//prompt("Easy, Medium or Hard").toLowerCase();
     difficulty.match('easy') ? moveBird(gameMode[0]) : difficulty.match('hard') ? moveBird(gameMode[2]) : moveBird(gameMode[1]);
 }
 function moveBird(difficulty){
+    let randomDist = (Math.random() * (2000 - 1500) + 1500); //specify random range for the distance
+    let randomTop = (Math.random() * (500 - (-300)) + (-300)); //specify random range for the height
     setTimeout(function(){
         $(birdOne).css({width: `${difficulty.size}`})
         $(birdOne).css({height: `${difficulty.size}`})
-        $(birdOne).animate({left: `${Math.random()*15000}px`})
-        $(birdOne).animate({top: `${Math.random()*500}px`})
+        $(birdOne).animate({left: `${randomDist}px`})
+        $(birdOne).animate({top: `${randomTop}px`})
         $(birdOne).css({transition: `${difficulty.speed}s`})
     },100)
 }
